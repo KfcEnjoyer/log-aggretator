c539d1b4e49ef955b49dd671be261d2561e8b9a2
zaproktnost <alikhan_azanbaev@mail.ru>
2025-02-25 17:30:44 -0500

    Added separate password struct and hashing

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,19 +1,38 @@
 package user
 
+import (
+	"auth-service/pkg/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var v = validator.NewValidator()
+
 type User interface {
 	LogIn() error
 	GetRole(username string) string
 }
 type RegularUser struct {
-	Id       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	Id       int      `json:"id"`
+	Username string   `json:"username"`
+	Password Password `json:"password"`
+	Role     string   `json:"role"`
 }
 
 type Admin struct {
 	RegularUser
 	Permissions []string
+}
+
+type Password struct {
+	Plain  string `json:"plain"`
+	Hashed string
+}
+
+func (p *Password) Hash() {
+	h, err := bcrypt.GenerateFromPassword([]byte(p.Plain), bcrypt.DefaultCost)
+	v.CheckError(err)
+
+	p.Hashed = string(h)
 }
 
 //

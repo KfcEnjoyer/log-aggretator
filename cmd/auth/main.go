00031918be2ca79d2af5fe7f27ea5a0cc5ced377
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-02-26 22:52:03 -0500

    Created login and home handlers

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"auth-service/internal/database"
+	database2 "auth-service/internal/database/log"
+	database1 "auth-service/internal/database/user_db"
 	"auth-service/internal/storage"
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	conn := database.CreateConnection()
+	conn := database.GetConnection()
 	if conn == nil {
 		log.Fatal("Failed to connect to the database")
 	}
 	defer conn.Close(context.Background())
 
-	database.CreateTable(conn)
+	database1.CreateTable()
+	database2.CreateTable()
 
 	port := "localhost:8080"
 	http.HandleFunc("/create", storage.Create)
+	http.HandleFunc("/login", storage.LogIn)
+	http.HandleFunc("/home", Home)
 
 	http.ListenAndServe(port, nil)
 
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello!")
 }

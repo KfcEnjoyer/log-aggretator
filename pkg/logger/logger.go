ec2cdcfe2ebcccfbe4301c5693c5c09b871439ae
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-02-26 22:51:09 -0500

    Extended Log type and added CreateLog function

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,10 +1,20 @@
 package logger
 
+import "time"
+
 type Log struct {
-	Id      int
-	Message string `json:"message"`
+	Id        int       `json:"id"`
+	TimeStamp time.Time `json:"TimeStamp"`
+	Level     string    `json:"level"`
+	Message   string    `json:"message"`
+	Username  string    `json:"username"`
+	Role      string    `json:"role"`
 }
 
-func (l *Log) CreateLog(map[string]string) {
-
+func (l *Log) CreateLog(t time.Time, level, message, username, role string) {
+	l.TimeStamp = t
+	l.Level = level
+	l.Message = message
+	l.Username = username
+	l.Role = role
 }

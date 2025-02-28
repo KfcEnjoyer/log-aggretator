f09b3fa62ee261b994b73eafca731e9e0f46efff
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-02-28 02:54:08 -0500

    Redesigned databases for easier readability

--- internal/database/log/log_db.go
+++ internal/database/log/log_db.go
@@ -5,21 +5,35 @@ import (
 	"auth-service/pkg/logger"
 	"context"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 )
 
-func CreateTable() {
-	conn := database.GetConnection()
-	query := `CREATE TABLE IF NOT EXISTS logs (id serial primary key, level varchar(20), message varchar(120), username varchar(60) unique, timestamp timestamptz NOT NULL default NOW())`
+type LogRepo interface {
+	CreateLog(l logger.Log) error
+}
 
-	_, err := conn.Exec(context.Background(), query)
-	if err != nil {
-		log.Fatalf("Query failed: %v", err)
+type LogRepoService struct {
+	dbPool *pgxpool.Pool
+}
+
+func NewLogRepoService() *LogRepoService {
+	return &LogRepoService{
+		dbPool: database.GetConnection(),
 	}
 }
 
-func CreateLog(l logger.Log) {
-	conn := database.GetConnection()
+//func CreateTable() {
+//	conn := database.GetConnection()
+//	query := `CREATE TABLE IF NOT EXISTS logs (id serial primary key, level varchar(20), message varchar(120), username varchar(60), timestamp timestamptz NOT NULL default NOW())`
+//
+//	_, err := conn.Exec(context.Background(), query)
+//	if err != nil {
+//		log.Fatalf("Query failed: %v", err)
+//	}
+//}
+
+func (r *LogRepoService) CreateLog(l logger.Log) error {
 	query := `INSERT INTO logs (level, message, username, timestamp) values (@level, @message, @username, @timestamp)`
 
 	args := pgx.NamedArgs{
@@ -29,8 +43,11 @@ func CreateLog(l logger.Log) {
 		"timestamp": l.TimeStamp,
 	}
 
-	_, err := conn.Exec(context.Background(), query, args)
+	_, err := r.dbPool.Exec(context.Background(), query, args)
 	if err != nil {
 		log.Fatalf("Query failed: %v", err)
+		return err
 	}
+
+	return nil
 }

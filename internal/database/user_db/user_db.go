7dfbcb1bed84dcae59e18d6e6ca87a65e438ee58
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-02-28 02:55:11 -0500

    Redesigned database files for easier readability

--- internal/database/user_db/user_db.go
+++ internal/database/user_db/user_db.go
@@ -5,82 +5,81 @@ import (
 	"auth-service/internal/user"
 	"context"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"strings"
 )
 
-func CreateTable() {
-	conn := database.GetConnection()
-	query := `CREATE TABLE IF NOT EXISTS users (id serial primary key, username varchar(60) unique, password varchar(60))`
+type UserRepo interface {
+	CreateUser(u user.RegularUser) error
+	GetUserByUsername(username string) (user.RegularUser, error)
+}
 
-	_, err := conn.Exec(context.Background(), query)
-	if err != nil {
-		log.Fatalf("Query failed: %v", err)
+type UserRepoService struct {
+	dbPool *pgxpool.Pool
+}
+
+func NewUserRepoService() *UserRepoService {
+	return &UserRepoService{
+		dbPool: database.GetConnection(),
 	}
 }
 
-func CreateUser(u user.RegularUser) {
-	conn := database.GetConnection()
-	query := `INSERT INTO users (username, password) values (@username, @password)`
+func (r *UserRepoService) CreateUser(u user.RegularUser) error {
+	query := `INSERT INTO users (username, password, role) values (@username, @password, @role)`
 
 	args := pgx.NamedArgs{
 		"username": strings.TrimSpace(u.Username),
 		"password": u.Password.Hashed,
+		"role":     u.Role,
 	}
-	_, err := conn.Exec(context.Background(), query, args)
+	_, err := r.dbPool.Exec(context.Background(), query, args)
 	if err != nil {
 		log.Fatalf("Query failed: %v", err)
-	}
-}
-
-func SelectUsers(users []string) {
-	conn := database.GetConnection()
-	query := `SELECT * FROM users`
-
-	info, err := conn.Query(context.Background(), query)
-	if err != nil {
-		log.Fatalf("Query failed: %v", err)
+		return err
 	}
 
-	defer info.Close()
+	return nil
 }
 
-func GetUser(username string) bool {
-	conn := database.GetConnection()
-	query := `SELECT username FROM users WHERE username=@username`
+//func CreateTable() {
+//	conn := database.GetConnection()
+//	query := `CREATE TABLE IF NOT EXISTS users (id serial primary key, username varchar(60) unique, password varchar(60), role varchar(20) default "regular"`
+//
+//	_, err := conn.Exec(context.Background(), query)
+//	if err != nil {
+//		log.Fatalf("Query failed: %v", err)
+//	}
+//}
+
+//func SelectUsers(users []string) {
+//	conn := database.GetConnection()
+//	query := `SELECT * FROM users`
+//
+//	info, err := conn.Query(context.Background(), query)
+//	if err != nil {
+//		log.Fatalf("Query failed: %v", err)
+//	}
+//
+//	defer info.Close()
+//}
+
+func (r *UserRepoService) GetUserByUsername(username string) (user.RegularUser, error) {
+	query := `SELECT * FROM users WHERE username=@username`
 
 	args := pgx.NamedArgs{
 		"username": username,
 	}
 
-	row := conn.QueryRow(context.Background(), query, args)
+	row := r.dbPool.QueryRow(context.Background(), query, args)
 
-	var u string
+	var u user.RegularUser
 
-	err := row.Scan(&u)
+	err := row.Scan(&u.Id, &u.Username, &u.Password.Hashed, &u.Role)
 	if err != nil {
 		log.Println("Error")
+		return user.RegularUser{}, err
 	}
 
-	return !(u == "")
-}
-
-func GetUserPassword(username string) string {
-	conn := database.GetConnection()
-	query := `SELECT password FROM users WHERE username=@username`
-
-	args := pgx.NamedArgs{
-		"username": username,
-	}
-
-	row := conn.QueryRow(context.Background(), query, args)
-
-	var p string
-
-	err := row.Scan(&p)
-	if err != nil {
-		log.Println("Error")
-	}
-
-	return p
+	return u, nil
 }

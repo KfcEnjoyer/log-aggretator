6b549fb63863ac6060ceec22d60f254b250ef108
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-02-28 02:54:32 -0500

    Changed main according to all the internal changes

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -2,29 +2,43 @@ package main
 
 import (
 	"auth-service/internal/database"
-	database2 "auth-service/internal/database/log"
-	database1 "auth-service/internal/database/user_db"
+	log2 "auth-service/internal/database/log"
+	"auth-service/internal/database/user_db"
+	"auth-service/internal/services"
 	"auth-service/internal/storage"
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	database.CreateConnection("C:\\Users\\user\\Desktop\\coding and stuff\\study or portfolio projects\\auth-service\\configs\\config.yaml")
+
 	conn := database.GetConnection()
+
 	if conn == nil {
 		log.Fatal("Failed to connect to the database")
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close()
 
-	database1.CreateTable()
-	database2.CreateTable()
+	userRepo := user_db.NewUserRepoService()
+	logRepo := log2.NewLogRepoService() // Assuming this function exists
+	auth := services.AuthService{
+		UserRepo: userRepo,
+		LogRepo:  logRepo,
+	}
 
 	port := "localhost:8080"
-	http.HandleFunc("/create", storage.Create)
-	http.HandleFunc("/login", storage.LogIn)
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		storage.Create(w, r, &auth)
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		storage.LogIn(w, r, &auth)
+	})
 	http.HandleFunc("/home", Home)
+	http.HandleFunc("/open", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("NFFFF")
+	})
 
 	http.ListenAndServe(port, nil)
 
